8b56e9e82af7f208ac62528da3f645ddf950cecb
heyaadarsh <aadarsh0001.be21@chitkara@edu.in>
2022-10-25 10:24:00 +0530

    Added introToVariables.js

---
+++ JS/introToVariables.js
@@ -0,0 +1,21 @@
+// Variables
+
+// Variables can store some information.
+// We can use that information later.
+// We can change that information later.
+
+// We have to use 'var' keyword to declare a variable in JS, but without using 'var' we can also declare the variable for preventing this we have to use the below line:
+"use strict";
+
+// Declairing a variable
+var myName = "Aadarsh Giri";
+// Variables Names are case sensitive.
+
+// Using Variable
+console.log(myName);
+
+// Changing Variable value
+myName = "Ayati";
+
+// Printing again
+console.log(myName);
