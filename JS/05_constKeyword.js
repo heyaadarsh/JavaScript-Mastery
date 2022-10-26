419562a78b35dd2dcffda714de1c53931e50231b
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-26 18:54:05 +0530

    const keyword file updated

--- JS/05_constKeyword.js
+++ JS/05_constKeyword.js
@@ -1,6 +1,7 @@
 // 'const' keyword is used for declaring the constant values.
 
-const pi = 3.14; // we can change this value again.
+const pi = 3.14; // we can't change this value again.
 console.log(pi);
+console.log(pi*2); // It is valid.
 
-pi = 1.47;
+pi = 1.47; // It will give an error.
