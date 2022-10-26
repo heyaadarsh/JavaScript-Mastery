012814f50c1c82a8741e1c3c8ccd4a566fcdb882
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-26 18:59:51 +0530

    String Indexing file added

---
+++ JS/06_ stringIndexing.js
@@ -0,0 +1,13 @@
+// String Indexing
+
+// A A D A R S H
+// 0 1 2 3 4 5 6
+
+let myName = "AADARSH";
+console.log(myName[0]); // It will print A
+console.log(myName[1]); // It will print A
+console.log(myName[2]); // It will print D
+console.log(myName[3]); // It will print A
+console.log(myName[4]); // It will print R
+console.log(myName[5]); // It will print S
+console.log(myName[6]); // It will print H
