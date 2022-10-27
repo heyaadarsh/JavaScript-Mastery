15ec054a960fa284f52da0f65012c5da3fa196e8
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-27 07:17:54 +0530

    String Number conversion File Added

--- JS/08_stringsAndNumbers.js
+++ JS/08_stringsAndNumbers.js
@@ -4,3 +4,13 @@ let myName = "Ayati";
 
 console.log(typeof number);
 console.log(typeof myName);
+
+// Number to String Conversion
+// Method-1
+number = number + ""; // It will convert 20 to "20"
+console.log(typeof number);
+
+// Method-2
+let value = 100;
+value = String(value);
+console.log(typeof value);
