1769c1c3c8b343cb2d725f226a3cdfc98679fdbb
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-27 07:05:44 +0530

    String Methods File Updated

--- JS/07_stringMethods.js
+++ JS/07_stringMethods.js
@@ -9,3 +9,11 @@ console.log(firstName.length); // It will remain same.
 // toUpperCase() - Used to capitalize the string.
 let myName = "narendra";
 console.log(myName.toUpperCase());
+
+// toLowerCase() - Coverts string into small letters same as toUpperCase()
+
+// slice - Used for getting some particular part from the string.
+let newName = "aaditya";
+// slice(starting_Index, End_Index + 1)
+let newString = newName.slice(0, 4);
+console.log(newString);
