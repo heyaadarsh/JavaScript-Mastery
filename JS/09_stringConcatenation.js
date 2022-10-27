a7e139c3e76042c27309c2e0a5245a7554444cc5
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-27 07:31:47 +0530

    String Concatenation File Added

---
+++ JS/09_stringConcatenation.js
@@ -0,0 +1,13 @@
+// Adding two or more strings
+let firstName = "Aadarsh";
+let lastName = "Giri"
+
+let fullName = firstName + " " + lastName; // " " - Used for giving space
+console.log(fullName);
+
+// Playing with types
+let val1 = "10";
+let val2 = "20";
+
+console.log(val1 + val2); // It will add as a string, so output will be "1020".
+console.log(+val1 + +val2); // Using "+" symbol we converted the string into the number, so the output will be 30.
