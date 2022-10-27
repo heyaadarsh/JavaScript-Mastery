46fcea13640e03db9887312f6571150bb7bfa4c1
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-27 06:56:10 +0530

    String Methods File Updated

---
+++ JS/07_stringMethods.js
@@ -0,0 +1,7 @@
+// trim() - Used to remove spaces from the strings.
+let firstName = "    AADARSH    ";
+console.log(firstName.length);
+let updatedName = firstName.trim();
+console.log(updatedName);
+console.log(updatedName.length);
+console.log(firstName.length); // It will remain same.
