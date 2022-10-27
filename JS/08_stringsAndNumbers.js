65a7bdf5e2ae16169603ce68ae40bc3286a21c0a
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2022-10-27 07:23:24 +0530

    String Number conversion File Added

--- JS/08_stringsAndNumbers.js
+++ JS/08_stringsAndNumbers.js
@@ -12,7 +12,7 @@ console.log(typeof number);
 
 // Method-2
 let value = 100;
-value = String(value);
+value = String(value); // It will convert 100 to "100".
 console.log(typeof value);
 
 // String to Number Conversion
@@ -23,5 +23,5 @@ console.log(typeof $value);
 
 // Method-2
 let _val = "77";
-_val = Number(_val);
+_val = Number(_val); // It will convert "77" to 77.
 console.log(typeof _val);
